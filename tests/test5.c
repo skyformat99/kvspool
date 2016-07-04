9bcbdf05abf0a980d8ab4ea7306911aabe79b075
Troy D. Hanson <tdh@tkhanson.net>
2016-07-04 15:18:06 -0400

    vtype

---
+++ tests/test5.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include "kvset.h"
+
+int main() {
+  kvset *set;
+  int l;
+
+  set = kvset_new();
+
+  l = kvset_len(set);
+  printf("set has %d items\n", l);
+
+  printf("adding two items to set\n");
+  kvset_add(set, "k1", 3, "v1", 3, 1);
+  kvset_add(set, "k2", 3, "v2", 3, 2);
+
+  l = kvset_len(set);
+  printf("set has %d items\n", l);
+
+  int i = 0, kl, vl, t;
+  char *k, *v;
+  while( (i = kvset_next(set, i, &k, &kl, &v, &vl, &t)) > 0) {
+    printf("%.*s -> %.*s (%d)\n", kl, k, vl, v, t);
+  }
+
+  printf("clearing set\n");
+  kvset_clear(set);
+
+  l = kvset_len(set);
+  printf("set has %d items\n", l);
+
+  printf("adding two items to set\n");
+  kvset_add(set, "k3", 3, "v3", 3, 3);
+  kvset_add(set, "k4", 3, "v4", 3, 4);
+
+  l = kvset_len(set);
+  printf("set has %d items\n", l);
+
+  i = 0;
+  while( (i = kvset_next(set, i, &k, &kl, &v, &vl, &t)) > 0) {
+    printf("%.*s -> %.*s (%d)\n", kl, k, vl, v, t);
+  }
+
+  kvset_free(set);
+
+  return 0;
+}
